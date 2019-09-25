f1d8341b85d2d6cdf87d3a3039012da8ca39f7fe
Jaideep Kekre <jaideepkekre@gmail.com>
2019-09-25 20:01:56 +0530

    neon-ness

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './App.css';
+import Typist from 'react-typist';
+import Typing from 'react-typing-animation';
 
 function App() {
   return (
     <div class="background">
-      <div class="logo">
-        <b>
-          Jai
+      <div class="logo">       
+          <b>
+            Jai
           <span>d</span>
-          ee 
+            ee
            <span>p </span>
-          Ke
+            Ke
           <span>K</span>
-          r
+            r
           <span>e</span>
-        </b>
+          </b>
       </div>
+      <div class="typer">
+      <Typing loop={true} speed={250}>
+        Python
+        <Typing.Backspace count={8} />
+        <Typing.Delay ms={1000} />
+        Go Lang
+        <Typing.Backspace count={8} />
+        <Typing.Delay ms={1000} />
+        ReactJS 
+        <Typing.Backspace count={8} />
+        <Typing.Delay ms={1000} />
+        Apache Spark
+        <Typing.Backspace count={13} />
+        <Typing.Delay ms={1000} />
+        Apache Kafka
+        <Typing.Backspace count={13} />
+        <Typing.Delay ms={1000} />
+        RabbitMQ
+        <Typing.Backspace count={13} />
+        <Typing.Delay ms={1000} />
+        Nginx
+        <Typing.Backspace count={13} />
+        </Typing>
+        </div>
+
     </div>
   );
 }
