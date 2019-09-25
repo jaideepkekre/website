07fad26b4399761cfb84e19b1726668e8fc2cf46
Jaideep Kekre <jaideepkekre@gmail.com>
2019-09-25 17:11:38 +0530

    Updates

---
+++ precache-manifest.d2374cc0e64b2103ef8e1712d0738bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab5ce889327faec64df59a86fab7e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8835e990ad0b56417de",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5522654a59c24549dcdf",
+    "url": "/static/js/2.f9e9fbfe.chunk.js"
+  },
+  {
+    "revision": "e8835e990ad0b56417de",
+    "url": "/static/js/main.10757603.chunk.js"
+  },
+  {
+    "revision": "105386bd44ea1df1540c",
+    "url": "/static/js/runtime-main.fc27b622.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
