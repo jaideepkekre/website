b251595e87c4772d97705ba4ad01c1fd5ccee68b
Jaideep Kekre <jaideepkekre@gmail.com>
2019-09-27 18:39:32 +0530

    slide show

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import './App.css';
 import Typist from 'react-typist';
 import Typing from 'react-typing-animation';
 
-function App() {
+
+var bg = ["https://media.giphy.com/media/l3UcrZHrGW2CjHXqM/giphy.gif",
+  "https://media.giphy.com/media/7bEpr3NIPNY0E/giphy.gif",
+  "https://media.giphy.com/media/k5zu35npVsYfgZQwwl/giphy.gif",
+  "https://media.giphy.com/media/pHZ8BBgLaXPliFNudh/giphy.gif",
+  "https://media.giphy.com/media/x45I8CY1Vs45G/giphy.gif",
+  "https://media.giphy.com/media/wKnqovL33x9in9ci6X/giphy.gif",
+]
+
+function getbackground() {
+  var min = 0
+  var max =5
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  var pos = Math.floor(Math.random() * (max - min + 1)) + min;
+  return bg[pos]
+}
+
+function App() {  
   return (
-    <div class="background">
+    <div class="background" style={{ backgroundImage: 'url(' + getbackground() + ')' }} >
       <div class="logo">       
           <b>
             Jai
@@ -42,7 +60,7 @@ function App() {
         <Typing.Backspace count={14} />
         </Typing>
       </div>
-      <audio src="https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3" type="audio/mpeg"  autoPlay />
+      {/* <audio src="https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3" type="audio/mpeg"  autoPlay /> */}
 
     </div>
   );
